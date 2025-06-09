053042d6c40054da3fe9104d7152a573948b6008
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-06-09 16:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const button = document.getElementById("btn");
+const age = document.getElementById("age");
+const name = document.getElementById("name");
+
+function btnfunction() {
+
+if (age.value=="" || name.value==""){
+	alert("Please enter valid details.");
+	return;
+}
+
+let promise = new Promise(function(resolve, reject) {
+  // asynchronous task
+	setTimeout(() => {
+	 if (age.value > 18) {
+        resolve(`Welcome,${name.value} . You can vote`);
+    } else {
+        reject(`Oh sorry ${name.value}. You aren't old enough.`);
+    }
+	},4000 );
+});
+ promise
+    .then(function (message) {
+      alert(message);
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+}
